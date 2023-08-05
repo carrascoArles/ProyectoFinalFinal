9529e2f0e7db5aabc04f8242aecd81d75746c806
Jean <jcharaco@unsa.edu.pe>
2023-08-05 16:17:51 -0500

    creacion de vista y home.html para mostrar los tatuadores y trabajos, autenticacion de usuario admin

--- Tattoo/views.py
+++ Tattoo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import ImagenTattoos,Tatuador
 
-# Create your views here.
+def vista_tatuajes(request):
+    tatuadores = Tatuador.objects.all()
+    tatuajes = ImagenTattoos.objects.all()
+    if  request.user.is_authenticated and request.user.is_staff:
+        is_staff = True
+    else:
+        is_staff = False
+    context ={
+      'tatuadores': tatuadores,
+      'tatuajes': tatuajes,
+      'is_staff': is_staff,
+    }
+    return render(request, 'home.html', context)
