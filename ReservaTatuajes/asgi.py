4be71584b27a446ae08f343f23e33e5728e6f78d
Reyser Julio Zapata Butron <rzapata@unsa.edu.pe>
2023-08-05 10:57:34 -0500

    cambio de proyecto - Reserva de Citas de Tatuaje

--- ReservaTatuajes/asgi.py
+++ ReservaTatuajes/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ECommerce project.
+ASGI config for ReservaTatuajes project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ECommerce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReservaTatuajes.settings')
 
 application = get_asgi_application()
