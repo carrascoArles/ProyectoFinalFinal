51a58b3ae809f22fd70e25fedb4fc734789d7f40
carrascoArles <acarrascoch@unsa.edu.pe>
2023-08-05 12:58:22 -0500

    agregado en models los tatuadores, imagenes por tatuador y el usuario

---
+++ Tattoo/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class Tatuador(models.Model):
+    nombre =models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=9,)
+    dni = models.CharField(max_length=8,)
+    yape = models.ImageField(upload_to='imagenes/')
+    ig = models.URLField()
+
+    def __str__(self):
+        txt ="{0} {1}"
+        return txt.format(self.nombre, self.apellido)
+
+class ImagenTattoos(models.Model):
+    tatuador =models.ForeignKey(Tatuador,null=False,on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='imagenes/')
+
+    def __str__(self):
+        txt ="Imagen N: {0} de {1}"
+        return txt.format(self.imagen, self.tatuador)
+
+class Usuario(models.Model):
+    nombre =models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=9,)
+
+    def __str__(self):
+        txt ="{0} {1}"
+        return txt.format(self.nombre, self.apellido)
+    
