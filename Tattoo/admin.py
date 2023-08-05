2d663f5981232657ba7889e3fd128a87d25467ab
carrascoArles <acarrascoch@unsa.edu.pe>
2023-08-05 13:07:47 -0500

    agregado el admin

---
+++ Tattoo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Tattoo.models import *
+# Register your models here.
+
+admin.site.register(Tatuador)
+admin.site.register(ImagenTattoos)
+admin.site.register(Usuario)
+
