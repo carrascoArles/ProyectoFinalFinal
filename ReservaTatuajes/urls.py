9b9fdcc3e8989d03f9727167d0d25800fd3e0805
Reyser Julio Zapata Butron <rzapata@unsa.edu.pe>
2023-08-05 17:49:58 -0500

    Merge y errores solucionados

--- ReservaTatuajes/urls.py
+++ ReservaTatuajes/urls.py
@@ -1,21 +1,7 @@
-"""
-URL configuration for ReservaTatuajes project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from Tattoo import views
 from Authentication.views import homeView, loginView, registerView
 from django.contrib.auth import views as auth_views
@@ -27,5 +13,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('register/', registerView, name='register'),
     path('tatuador/',views.Tatuador, name='assag'),
+
+    path ('', include('Tattoo.urls')),
+    path('tatuador/',views.crear_tatuador, name='assag'),
     path('imagentatu/',views.Imagenes, name='assag'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
--- Tattoo/views.py
+++ Tattoo/views.py
@@ -1,19 +1,62 @@
-from django.shortcuts import render, redirect
+from .models import ImagenTattoos,Tatuador
+from django.shortcuts import render, redirect,get_object_or_404
+from django.core.mail import send_mail
 from .forms import *
 
-def Tatuador(request):
+def vista_tatuajes(request):
+    tatuadores = Tatuador.objects.all()
+    tatuajes = ImagenTattoos.objects.all()
+    if  request.user.is_authenticated and request.user.is_staff:
+        is_staff = True
+    else:
+        is_staff = False
+    context ={
+      'tatuadores': tatuadores,
+      'tatuajes': tatuajes,
+      'is_staff': is_staff,
+    }
+    return render(request, 'home.html', context)
+def pedir_cita(request, tatuador_dni):
+    tatuador = get_object_or_404(Tatuador, dni=tatuador_dni)
+    envio_exitoso = "" 
+    if request.method == 'POST':
+        user = request.user
+        asunto = f"[{user}] {request.POST['asunto']}"
+        mensaje = request.POST['mensaje']
+        destinatario = tatuador.correo
+        envio_exitoso = "El correo se ha enviado exitosamente."
 
-    tattooForm = TattooForm()
+        send_mail(asunto, mensaje, 'correo empresa', [destinatario])
+    context = {
+        'tatuador': tatuador,
+        'envio_exitoso': envio_exitoso
+    }
+    return render(request, 'pedir_cita.html', context)
+
+def crear_tatuador(request):
+    if request.method == 'POST':
+        tattooForm = TattooForm(request.POST, request.FILES)
+        if tattooForm.is_valid():
+            tattooForm.save()
+            return redirect('http://127.0.0.1:8000/imagentatu/')
+    else:
+        tattooForm = TattooForm()
 
     return render(request, 'tatuador.html', {'form': tattooForm})
 
 def Imagenes(request):
 
-    sub_Form = ImagenForm()
+    if request.method == 'POST':
+        tattooForm = ImagenForm(request.POST, request.FILES)
+        if tattooForm.is_valid():
+            tattooForm.save()
+            return redirect('http://127.0.0.1:8000/tatuador/')
+    else:
+        tattooForm = ImagenForm()
 
-    return render(request, 'imagen.html', {'form1': sub_Form})
+    return render(request, 'tatuador.html', {'form1': tattooForm})
 
-def Usuario(request):
+def crear_usuario(request):
 
     sub_Form = Usuario()
 
